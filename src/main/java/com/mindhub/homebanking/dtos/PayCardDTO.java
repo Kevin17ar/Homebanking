953c06966ea7e302b9aef5cf12a1c5d048a115e9
Kevin17ar <81791483+Kevin17ar@users.noreply.github.com>
2021-08-27 00:41:37 -0300

    Update PayCardDTO.java

--- src/main/java/com/mindhub/homebanking/dtos/PayCardDTO.java
+++ src/main/java/com/mindhub/homebanking/dtos/PayCardDTO.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 public class PayCardDTO {
-    private String number;
+    private String cardNumber;
     private int cvv;
     private double amount;
     private String description;
@@ -13,8 +13,8 @@ public class PayCardDTO {
     public PayCardDTO() {
     }
 
-    public PayCardDTO(String number, int cvv, double amount, String description) {
-        this.number = number;
+    public PayCardDTO(String cardNumber, int cvv, double amount, String description) {
+        this.cardNumber = cardNumber;
         this.cvv = cvv;
         this.amount = amount;
         this.description = description;
